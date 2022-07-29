6c4b7d17e4bfd31828f4a5305fdd281886fe5142
Hemant Kumar <mldlpractice@gmail.com>
2022-07-29 13:27:35 +0530

    chane procfile

--- app.py
+++ app.py
@@ -1,23 +1,14 @@
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
 import requests
-from telegram import ChatAction
+from flask import Flask
 import os
 
 
-
-def hello(update: Update, context: CallbackContext) -> None:
-    intro_text = """
-    🤖 Greetings human! \n
-    🤗 I'm a bot hosted on Hugging Face Spaces. \n
-    🦾 I can query the mighty GPT-J-6B model and send you a response here. Try me.\n
-    ✉️ Send me a text to start and I shall generate a response to complete your text!\n\n
-    ‼️ PS: Responses are not my own (everything's from GPT-J-6B). I'm not conscious (yet).\n
-    """
-    update.message.reply_text(intro_text)
+app = Flask(__name__)
 
 
+@app.route('/')
 def get_gpt_response(text):
+
     r = requests.post(
         url="https://hf.space/embed/singh5587/gpt-j-6b/+/api/predict ",
         json={"data": [text]},
@@ -25,15 +16,10 @@ def get_gpt_response(text):
     response = r.json()
     return response["data"][0]
 
-def respond_to_user(update: Update, context: CallbackContext):
-    update.message.chat.send_action(action=ChatAction.TYPING)
-    response_text = get_gpt_response(update.message.text)
-    update.message.reply_text(response_text)
+
+get_gpt_response('hello')
 
 
-updater = Updater('5533872077:AAHwJ-zkpfjWc1sVb1BHRQFci2DczTaEVGI')
-updater.dispatcher.add_handler(CommandHandler('start', hello))
-updater.dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, respond_to_user))
-updater.start_polling()
-updater.idle()
 
+if __name__ == "__main__":
+    app.run()
---
+++ bot.py
@@ -0,0 +1,33 @@
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
+from telegram import ChatAction
+import os
+from app import get_gpt_response
+
+
+
+def hello(update: Update, context: CallbackContext) -> None:
+    intro_text = """
+    🤖 Greetings human! \n
+    🤗 I'm a bot hosted on Hugging Face Spaces. \n
+    🦾 I can query the mighty GPT-J-6B model and send you a response here. Try me.\n
+    ✉️ Send me a text to start and I shall generate a response to complete your text!\n\n
+    ‼️ PS: Responses are not my own (everything's from GPT-J-6B). I'm not conscious (yet).\n
+    """
+    update.message.reply_text(intro_text)
+
+
+
+
+def respond_to_user(update: Update, context: CallbackContext):
+    update.message.chat.send_action(action=ChatAction.TYPING)
+    response_text = get_gpt_response(update.message.text)
+    update.message.reply_text(response_text)
+
+
+updater = Updater('5533872077:AAHwJ-zkpfjWc1sVb1BHRQFci2DczTaEVGI')
+updater.dispatcher.add_handler(CommandHandler('start', hello))
+updater.dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, respond_to_user))
+updater.start_polling()
+updater.idle()
+
